cf6522826c4c9502204246317468a2259fc6f142
Timothy Smith <timothytsmith8@gmail.com>
2021-01-26 20:42:12 -0800

    express dependency created and a connection created

---
+++ server.js
@@ -0,0 +1,20 @@
+// express dependency
+const express = require('express');
+// calls express 
+ const app = express();
+
+ const PORT = process.env.PORT || 3001;
+
+// For: data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"))
+
+
+
+
+
+//port listener
+app.listen(PORT, () => {
+    console.log("App listening on PORT " + PORT);
+});
